fc7541d19b7747a78be1b2276f9f6c05bed6bb38
Donraf <maxim.anikin1@yandex.ru>
2025-02-02 18:26:59 +0300

    ChartData improving

--- client/src/utils/ChartData.js
+++ client/src/utils/ChartData.js
@@ -1,18 +1,28 @@
 export class ChartData {
-    constructor(initIndex = 0, maxPointsInSet = 10) {
+    constructor(initIndex = 0, maxPointsInSet = 10, criticalValue = 0.9, checkDangerNum = 3) {
         this.curIndex = initIndex;
         this.maxPointsInSet = maxPointsInSet;
+        this.criticalValue = criticalValue;
+        this.checkDangerNum = checkDangerNum;
         this.points = [];
         this.data = {
             labels: this.points.map( point => { return point.x }),
             datasets: [
                 {
                     type: 'line',
-                    label: 'Dataset 1',
-                    borderColor: 'rgb(255, 99, 132)',
+                    label: 'Значение процесса',
+                    borderColor: 'rgb(0, 0, 0)',
                     borderWidth: 2,
                     fill: false,
                     data: this.points.map( point => { return point.y } ),
+                },
+                {
+                    type: 'line',
+                    label: 'Критическое значение процесса',
+                    borderColor: 'rgb(255, 0, 60)',
+                    borderWidth: 2,
+                    fill: false,
+                    data: this.points.map( point => { return this.criticalValue } ),
                 }
             ]
         }
@@ -29,16 +39,44 @@ export class ChartData {
             datasets: [
                 {
                     type: 'line',
-                    label: 'Dataset 1',
-                    borderColor: 'rgb(255, 99, 132)',
+                    label: 'Значение процесса',
+                    borderColor: 'rgb(0, 0, 0)',
                     borderWidth: 2,
                     fill: false,
                     data: this.points.map( point => { return point.y } ),
+                },
+                {
+                    type: 'line',
+                    label: 'Критическое значение процесса',
+                    borderColor: 'rgb(255, 0, 60)',
+                    borderWidth: 2,
+                    fill: false,
+                    data: this.points.map( point => { return this.criticalValue } ),
                 }
             ]
         }
         return this.data
     }
+
+    // getCurrentValue() {
+    //     return this.data.datasets[]
+    // }
+    //
+    // getCriticalValue() {
+    //     return this.criticalValue
+    // }
+
+    isCrashed(index = this.data.datasets[0].data.length - 1) {
+        return this.data.datasets[0].data[index] >= this.criticalValue
+    }
+
+    isDanger() {
+        let isDanger = false;
+        for (let i = this.points.length - 1; i > this.points.length - this.checkDangerNum - 1 && i >= 0; i--) {
+            isDanger = isDanger || this.isCrashed(i);
+        }
+    }
+
 }
 
 class Point {
