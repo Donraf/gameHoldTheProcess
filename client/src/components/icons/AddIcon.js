61418b602607416c38fbc63a10ab229e65be895b
Donraf <maxim.anikin1@yandex.ru>
2025-08-14 22:01:27 +0300

    Client: icon fix

--- client/src/components/icons/AddIcon.js
+++ client/src/components/icons/AddIcon.js
@@ -1,7 +1,7 @@
 export default function AddIcon() {
     return (
         <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path d="M4 12H20M12 4V20" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M4 12H20M12 4V20" stroke="#ffffff" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
     </svg>
     );
 }
