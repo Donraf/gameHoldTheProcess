a39c604168227dd219f60814f56da7cb4d84b285
Аникин Максим <maxim.anikin1@yandex.ru>
2025-10-25 21:36:50 +0300

    Server: changed database deletion behaviour

---
+++ server/schema/000003_init.down.sql
@@ -0,0 +1,23 @@
+ALTER TABLE ONLY User_Groups
+    DROP CONSTRAINT user_groups_user_id_fkey;
+ALTER TABLE ONLY User_Groups
+    ADD CONSTRAINT user_groups_user_id_fkey 
+    FOREIGN KEY (user_id) REFERENCES Users (user_id);
+
+ALTER TABLE ONLY User_Parameter_Sets
+    DROP CONSTRAINT user_parameter_sets_user_id_fkey;
+ALTER TABLE ONLY User_Parameter_Sets
+    ADD CONSTRAINT user_parameter_sets_user_id_fkey 
+    FOREIGN KEY (user_id) REFERENCES Users (user_id);
+
+ALTER TABLE ONLY Charts
+    DROP CONSTRAINT charts_user_id_fkey;
+ALTER TABLE ONLY Charts
+    ADD CONSTRAINT charts_user_id_fkey 
+    FOREIGN KEY (user_id) REFERENCES Users (user_id) ON DELETE NO ACTION;
+
+ALTER TABLE ONLY Statistics
+    DROP CONSTRAINT statistics_user_id_fkey;
+ALTER TABLE ONLY Statistics
+    ADD CONSTRAINT statistics_user_id_fkey 
+    FOREIGN KEY (user_id) REFERENCES Users (user_id);
